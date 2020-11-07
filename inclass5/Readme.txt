08d0e11c43201c484e75bc5bbfc3eb60974597cc
alexmeier2828 <ameier42@gmail.com>
2020-11-07 12:16:30 -0500

    in class 5, fixed missing file

---
+++ inclass5/Readme.txt
@@ -0,0 +1,2 @@
+Link to github deployment: 
+https://alexmeier2828.github.io/inclass5/example.html
--- inclass5/js/example.js
+++ inclass5/js/example.js
@@ -1,10 +1,24 @@
+var page = document.getElementById("page");
+var pageDescendents = page.children;
+var table  = pageDescendents[2];
 // ADD NEW ITEM TO END OF LIST
-
+var listItemOnEnd = document.createElement('li');
+listItemOnEnd.innerHTML = 'cream'; 
+table.appendChild(listItemOnEnd);
 
 // ADD NEW ITEM START OF LIST
-
+var listItemAtBeginning = document.createElement('li');
+listItemAtBeginning.innerHTML = "kale"
+table.prepend(listItemAtBeginning);
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
-
+table.childNodes.forEach(li => {
+    console.log(li.classList = "cool");
+});
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var numberOfItems = table.childElementCount;
+var itemCountelement = document.createElement("p");
+itemCountelement.innerHTML = numberOfItems;
+var header = pageDescendents[1];
+header.innerText = header.innerText + " " + numberOfItems;
