a92b554ef5f0c119dde60f5b75d4e6b3dc6ea0b0
alexmeier2828 <ameier42@gmail.com>
2020-11-19 11:03:48 -0500

    added sliders with cross binding

---
+++ HW7/js/sliders.js
@@ -0,0 +1,25 @@
+(function($){
+    //initialize sliders
+    var sliderOptions = {
+        slide: function (e, ui) {
+            var val = ui.value; 
+            console.log("SliderMoved:" + $(this).id + " " + val);
+            $(this).next().val(val);
+        },
+        value: $(this).next().val(),
+        animate: true,
+        min: -50,
+        max: 50,
+    }
+    $("#rowStartSlider").slider(sliderOptions);
+    $("#rowEndSlider").slider(sliderOptions);
+    $("#colStartSlider").slider(sliderOptions);
+    $("#colEndSlider").slider(sliderOptions);
+
+
+    //crossbinding for input fields 
+    $(".formNumber").change(function (event) {
+        $(this).prev().slider('value', event.target.value) //set associated slider value to the value of this input field
+    });
+
+})(jQuery);
