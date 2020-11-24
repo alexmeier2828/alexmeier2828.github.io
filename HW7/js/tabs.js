682f60d6c7ecbe4711046430a64f8423362f8a76
alexmeier2828 <ameier42@gmail.com>
2020-11-24 13:55:26 -0500

    Changed theme, added checkbox with styling, added commments for tabs.js

--- HW7/js/tabs.js
+++ HW7/js/tabs.js
@@ -1,20 +1,23 @@
+/*
+Author: Alex Meier
+Email: alex_meier@student.uml.edu
+*/
 (function ($){
+    //initialize tabs
     var tabOpts = {
         selected: 1
     }
     $("#tabContainer").tabs(tabOpts);
 
-    $("#tabsContainer").bind("tabsadd", function(e, tab) {
-        $("#tabsContainer").tabs("select", -1);
-    });
-
+    //on click event listener for delete tab button 
     $(document).on("click", ".delete" ,function() {
         var tab = $(this).parent().attr("id");
-        $("#" + tab).remove();
-        $("#tab-" + tab).remove();
-        $("#tabsContainer").tabs("refresh");
+        $("#" + tab).remove();                      //remove tab body 
+        $("#tab-" + tab).remove();                  //remove tab from tray
+        $("#tabsContainer").tabs("refresh");        //refresh tab framework
     })
 
+    //event listener for delete all 
     $("#delete-all").click(function (e) { 
         $("#tabList").empty();
         $(".tabBody").remove();
