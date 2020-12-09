bedfd90d1a87cc01d425c4323a24cf9dfbaeb2cf
alexmeier2828 <ameier42@gmail.com>
2020-12-09 16:47:00 -0500

    two way draggables work

--- HW8/js/dragableTiles.js
+++ HW8/js/dragableTiles.js
@@ -4,10 +4,30 @@ $(function (){
     };
     $(".tile").draggable(tileOptions);
 
+    $("#holder").droppable({
+        drop: function(event, ui){
+            if(ui.draggable.parent().attr('id') !== 'holder'){
+                ui.draggable.parent().droppable('enable');
+
+            }
+            $(this).append(ui.draggable);
+            if($(this).hasClass("board-space")){
+                $(this).droppable('disable');
+            }
+            console.log(JSON.stringify(event.draggable));
+        }
+    })
     $(".board-space").droppable({
         drop: function (event, ui){
-            $(this).addClass("ui-state-highlight");
-            console.log($(this))
-        }
+            if(ui.draggable.parent().attr('id') !== 'holder'){
+                ui.draggable.parent().droppable('enable');
+
+            }
+            $(this).append(ui.draggable);
+            if($(this).hasClass("board-space")){
+                $(this).droppable('disable');
+            }
+            console.log(JSON.stringify(event.draggable));
+        },
     });
 });
