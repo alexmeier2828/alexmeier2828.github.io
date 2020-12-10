7026fde3f60424ca356dc8640d08a8cc8bb55998
alexmeier2828 <ameier42@gmail.com>
2020-12-09 20:42:40 -0500

    tiles can only be place next to eachother

--- HW8/js/dragableTiles.js
+++ HW8/js/dragableTiles.js
@@ -1,4 +1,18 @@
+function isPlayableSpot(spot, firstPiecePlayed){
+    if(!firstPiecePlayed){
+        return true; 
+    }
+    
+    var leftSide = spot.prev();
+    var rightSide = spot.next();
+    
+    //check if one neighboring spot has a piece in it (given this is not the first)
+    return (leftSide !== null && leftSide.hasClass('occupied') || (rightSide!== null && rightSide.hasClass('occupied')))
+
+}
+
 $(function (){
+    var firstPiecePlayed = false;
     var tileOptions = {
         revert:true
     };
@@ -8,26 +22,37 @@ $(function (){
         drop: function(event, ui){
             if(ui.draggable.parent().attr('id') !== 'holder'){
                 ui.draggable.parent().droppable('enable');
-
+                ui.draggable.parent().removeClass('occupied')
             }
             $(this).append(ui.draggable);
-            if($(this).hasClass("board-space")){
-                $(this).droppable('disable');
-            }
+            
+            //check if the last peice is being removed
+            firstPiecePlayed = $(this).siblings().hasClass('occupied');
+
+            // $(this).siblings().forEach(element => {
+            //     if(element.hasClass('occupied')){
+            //         firstPiecePlayed = true;
+            //     }
+            // });
             console.log(JSON.stringify(event.draggable));
         }
     })
     $(".board-space").droppable({
         drop: function (event, ui){
-            if(ui.draggable.parent().attr('id') !== 'holder'){
-                ui.draggable.parent().droppable('enable');
-
-            }
-            $(this).append(ui.draggable);
-            if($(this).hasClass("board-space")){
+            if(isPlayableSpot($(this), firstPiecePlayed)){
+                if(!firstPiecePlayed){
+                    firstPiecePlayed = true;
+                }
+                if(ui.draggable.parent().attr('id') !== 'holder'){
+                    ui.draggable.parent().droppable('enable');
+                    ui.draggable.parent().removeClass('occupied')
+                }
+                $(this).append(ui.draggable);
+                $(this).addClass('occupied');
                 $(this).droppable('disable');
+                console.log(JSON.stringify(event.draggable));
             }
-            console.log(JSON.stringify(event.draggable));
+
         },
     });
 });
